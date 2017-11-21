2427964064feaa76f404f26f8c430d1196248361
MacRJ <rmacwj@gmail.com>
2017-11-20 20:11:47 -0700

    added total feature

--- src/BillComponents/billBody.js
+++ src/BillComponents/billBody.js
@@ -2,25 +2,54 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import {Item} from './index'
 
 
 class BillBody extends Component{
 
-  getDescription() {
-    return this.props.bill.map(single => {
-      return <Text>{single.name}</Text>
-    })
+state = {
+  total: null
+}
+
+total() {
+
+}
+
+renderItems() {
+  var arr = this.props.bill
+  var currentTab = arr.reduce((accu, item, id) => {
+    var foundIdx = deepIncludes(accu, item)
+    if(foundIdx === -1) {
+      accu.push({drink_id: item.drink_id, name: item.name, fulfilled: item.fulfilled, price: item.price, quantity: 1})
+    } else {
+      accu[foundIdx].quantity ++
+    }
+    return accu
+  }, [])
+  function deepIncludes(arr, obj) {
+    for(var i = 0; i< arr.length; i++) {
+      if(arr[i].drink_id === obj.drink_id) {
+        return i
+      }
+    }
+    return -1
   }
+  return currentTab.map((tab, i) => {
+    return <Item
+    currentTab={tab}
+    key={i}
+    />
+  })
+}
+
+
+
   render() {
+    const {body, description} = styles
 
-
-  const {body} = styles
-  return (
+    return (
     <View style = {body}>
-      <Text></Text>
-      {this.getDescription()}
-      <Text> Quantiy </Text>
-      <Text> Total </Text>
+      {this.renderItems()}
     </View>
   )
 }
@@ -30,12 +59,9 @@ const styles = {
   body: {
     flex: 2,
     paddingTop: 10,
-    justifyContent: 'space-between',
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignItems: 'flex-start',
     borderBottomWidth: 2,
-
-
   }
 }
 
---
+++ src/BillComponents/item.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native'
+
+class Item extends Component {
+
+  getTotal() {
+    const {quantity, price, name} = this.props.currentTab
+    let price2 = parseInt(price)
+    let total = 0
+    if(quantity > 1) {
+      for(var i =0; i < quantity; i++){
+          total = price2 + total
+      }
+    } else {return price}
+    return total
+  }
+
+
+  render() {
+    const {body, one, two, three, four} = styles
+  return (
+    <View style={body}>
+      <Text style={one}>{null}</Text>
+      <Text style={two}> {this.props.currentTab.name}</Text>
+      <Text style= {three}> {this.props.currentTab.quantity} </Text>
+      <Text style={four}> $ {this.getTotal()} </Text>
+    </View>
+  )
+}
+}
+
+const styles = {
+  body: {
+    flexDirection: 'row',
+  },
+  one: {
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  two: {
+    flex: 1
+  },
+  three: {
+    flex: 1
+  },
+  four: {
+    justifyContent: 'flex-end'
+  }
+}
+
+export default Item
